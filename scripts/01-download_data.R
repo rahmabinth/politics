5afdd0e54e8ca85fcf980617587fa4792007ed34
Rahma Binth Mohammad <rahmabinth@gmail.com>
2024-03-05 22:41:02 -0500

    First commit.

---
+++ scripts/01-download_data.R
@@ -0,0 +1,29 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(dataverse)
+
+#### Download data ####
+ces2020 <-
+  get_dataframe_by_name(
+    filename = "CES20_Common_OUTPUT_vv.csv",
+    dataset = "10.7910/DVN/E9N6PH",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) |>
+  select(votereg, CC20_410, gender, educ)
+
+write_csv(ces2020, "ces2020.csv")
+
+
+
+         
